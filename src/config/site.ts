80024f4c758fd22319d1f6613bc31c45f3f4f9f6
rikfkiahmadfahrezi <rifkiaf589@gmail.com>
2025-04-13 18:33:42 +0700

    get site url from env

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,9 +1,11 @@
+const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000"
+
 export const siteConfig = {
    title: "IMPHNEN",
    description: "Komunitas ingin menjadi programmer handal namun enggan ngoding.",
    keyword: ["imphnen", "komunitas programmer", "ngoding"],
-   url: "https://imphnen.rifkiaf.my.id",
-   openGraph: "https://imphnen.rifkiaf.my.id/og.jpg",
+   url: SITE_URL,
+   openGraph: `${SITE_URL}/og.jpg`,
    links: {
       facebook: "https://web.facebook.com/groups/programmerhandal/",
       discord: "https://discord.gg/imphnen",
